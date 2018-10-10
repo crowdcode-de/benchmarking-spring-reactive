8349daf427ca3f05b20a6806f372540280998f17
Ingo Düppe <ingo.dueppe@crowdcode.io>
2018-10-10 22:51:18 +0200

    add repository annotation

--- cddb-blocking-service/src/main/java/io/crowdcode/blocking/cddb/AlbumMongoRepository.java
+++ cddb-blocking-service/src/main/java/io/crowdcode/blocking/cddb/AlbumMongoRepository.java
@@ -2,10 +2,12 @@ package io.crowdcode.blocking.cddb;
 
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AlbumMongoRepository extends MongoRepository<Album, String> {
 
     Optional<Album> findByDiscId(String discId);
